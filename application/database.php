05c5c1f6af832219be67bc93eafe7f4b5baa045f
刘江波 <1163686423@qq.com>
2020-05-28 23:28:22 +0800

    修改

--- application/database.php
+++ application/database.php
@@ -1,15 +1,16 @@
 <?php
+
 use think\Env;
 
 return array(
-	'type'   =>'mysql',
-	'hostname'   =>Env::get('DATABASE_HOSTNAME'),
-	'hostport'   =>'3306',
-	'database'   =>Env::get('DATABASE_NAME'),
-	'username'   =>Env::get('DATABASE_USERNAME'),
-	'password'   =>Env::get('DATABASE_PASSWORD'),
-	'prefix'     =>'lz_',
-	'charset'    => 'utf8',
-	'resultset_type' => 'array',
-	'fields_strict'  => true,
+    'type' => 'mysql',
+    'hostname' => Env::get('DATABASE_HOSTNAME'),
+    'hostport' => Env::get('DATABASE_PORT'),
+    'database' => Env::get('DATABASE_NAME'),
+    'username' => Env::get('DATABASE_USERNAME'),
+    'password' => Env::get('DATABASE_PASSWORD'),
+    'prefix' => 'lz_',
+    'charset' => 'utf8',
+    'resultset_type' => 'array',
+    'fields_strict' => true,
 );
